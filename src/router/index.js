ac1c3d625ace6b7297ec570b4313e54069f9ca5d
Bego Blanco <begoblanfer@gmail.com>
2024-08-05 14:51:52 +0200

    basic structure

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,54 @@
 import { createRouter, createWebHistory } from "vue-router";
-import LoginView from "../views/guards/LoginView.vue";
+import Login from "../views/guards/LoginView.vue";
+import Dashboard from "../views/DashboardView.vue";
+import Forms from "../views/Forms.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    // {
+    //   path: "/",
+    //   name: "Login",
+    //   component: LoginView,
+    //   meta: { title: "Zootopia" },
+    // },
+    // {
+    //   path: "/home",
+    //   name: "Dashboard",
+    //   component: DashboardView,
+    //   meta: { title: "Zootopia" },
+    // },
     {
-      path: "/",
-      name: "Login",
-      component: LoginView,
-      meta: { title: "Zootopia" },
+      path: '/',
+      name: 'Login',
+      component: Login,
+      meta: { layout: 'empty' },
     },
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: Dashboard,
+    },
+    {
+      path: '/forms',
+      name: 'Forms',
+      component: Forms,
+    },
+    // {
+    //   path: '/cards',
+    //   name: 'Cards',
+    //   component: Card,
+    // },
+    // {
+    //   path: '/tables',
+    //   name: 'Tables',
+    //   component: Tables,
+    // },
   ],
 });
 
 router.beforeEach((to, from, next) => {
-  document.title = to.meta.title || "SupportApp";
+  document.title = to.meta.title || "Zootopia";
   next();
 });
 
