dcb4b59ae12c059b6a453d22c040d8156b792846
Bego Blanco <begoblanfer@gmail.com>
2024-08-10 21:47:20 +0200

    fixed animal store

--- src/stores/animalStore.js
+++ src/stores/animalStore.js
@@ -1,13 +1,4 @@
-// import { defineStore } from "pinia";
-// import axios from "axios";
 
-// const createApi = (baseURL = "http://localhost:8080/api/animals") => {
-//   const api = axios.create({
-//     baseURL,
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//   });
 
 //   api.interceptors.response.use(
 //     (response) => response,
@@ -36,83 +27,13 @@
 //   return api;
 // };
 
-// export const useAnimalStore = defineStore({
-//   id: "animal",
-//   state: () => ({
-//     animals: [],
-//     error: null,
-//     isLoading: false,
-//   }),
-//   getters: {
-//     getRequestById: (state) => (id) => state.animals.find((req) => req.id === id),
-//     totalRequests: (state) => state.animals.length,
-//   },
-//   actions: {
-//     async fetchAllAnimals(api = createApi()) {
-//       this.isLoading = true;
-//       try {
-//         const { data } = await api.get("");
-//         this.animals = data;
-//       } catch (error) {
-//         this.error = error.message;
-//       } finally {
-//         this.isLoading = false;
-//       }
-//     },
-
-//     async createAnimal(newAnimal, api = createApi()) {
-//       this.isLoading = true;
-//       try {
-//         const { data } = await api.post("", newAnimal);
-//         this.animals.push(data);
-//       } catch (error) {
-//         this.error = error.message;
-//       } finally {
-//         this.isLoading = false;
-//       }
-//     },
-
-//     async updateAnimal(id, updatedAnimal, api = createApi()) {
-//       this.isLoading = true;
-//       try {
-//         const { data } = await api.put(`/${id}`, updatedAnimal);
-//         const index = this.animals.findIndex((req) => req.id === id);
-//         if (index !== -1) {
-//           this.animals[index] = data;
-//         } else {
-//           throw new Error(`Request with id ${id} not found in local state.`);
-//         }
-//       } catch (error) {
-//         this.error = error.message;
-//       } finally {
-//         this.isLoading = false;
-//       }
-//     },
-
-//     async deleteAnimal(id, api = createApi()) {
-//       this.isLoading = true;
-//       try {
-//         await api.delete(`/${id}`);
-//         this.animals = this.animals.filter((req) => req.id !== id);
-//       } catch (error) {
-//         this.error = error.message;
-//       } finally {
-//         this.isLoading = false;
-//       }
-//     },
-
-//     clearError() {
-//       this.error = null;
-//     },
-//   },
-// });
 
 
 import { defineStore } from 'pinia';
 import axios from 'axios';
 
 
-const BASE_URL = 'http://localhost:8080/animals';
+const BASE_URL = 'http://localhost:8080/api/v1/animals';
 
 const api = axios.create({
   baseURL: BASE_URL,
@@ -208,83 +129,83 @@ export const useAnimalStore = defineStore('animal', {
       }
     },
 
-    async addFamily(newFamily) {
-      this.isLoading = true;
-      try {
-        await api.post('/families/add', newFamily);
-        this.fetchFamilies(); 
-      } catch (error) {
-        this.error = 'Error adding family: ' + error.message;
-        console.error(this.error);
-      } finally {
-        this.isLoading = false;
-      }
-    },
+  //   async addFamily(newFamily) {
+  //     this.isLoading = true;
+  //     try {
+  //       await api.post('/families/add', newFamily);
+  //       this.fetchFamilies(); 
+  //     } catch (error) {
+  //       this.error = 'Error adding family: ' + error.message;
+  //       console.error(this.error);
+  //     } finally {
+  //       this.isLoading = false;
+  //     }
+  //   },
 
-    async updateFamily(id, updatedFamily) {
-      this.isLoading = true;
-      try {
-        await api.put(`/families/update/${id}`, updatedFamily);
-        this.fetchFamilies(); 
-      } catch (error) {
-        this.error = `Error updating family with ID ${id}: ` + error.message;
-        console.error(this.error);
-      } finally {
-        this.isLoading = false;
-      }
-    },
+  //   async updateFamily(id, updatedFamily) {
+  //     this.isLoading = true;
+  //     try {
+  //       await api.put(`/families/update/${id}`, updatedFamily);
+  //       this.fetchFamilies(); 
+  //     } catch (error) {
+  //       this.error = `Error updating family with ID ${id}: ` + error.message;
+  //       console.error(this.error);
+  //     } finally {
+  //       this.isLoading = false;
+  //     }
+  //   },
 
-    async deleteFamily(id) {
-      this.isLoading = true;
-      try {
-        await api.delete(`/families/delete/${id}`);
-        this.fetchFamilies(); 
-      } catch (error) {
-        this.error = `Error deleting family with ID ${id}: ` + error.message;
-        console.error(this.error);
-      } finally {
-        this.isLoading = false;
-      }
-    },
+  //   async deleteFamily(id) {
+  //     this.isLoading = true;
+  //     try {
+  //       await api.delete(`/families/delete/${id}`);
+  //       this.fetchFamilies(); 
+  //     } catch (error) {
+  //       this.error = `Error deleting family with ID ${id}: ` + error.message;
+  //       console.error(this.error);
+  //     } finally {
+  //       this.isLoading = false;
+  //     }
+  //   },
 
-    async addType(newType) {
-      this.isLoading = true;
-      try {
-        await api.post('/types/add', newType);
-        this.fetchTypes(); 
-      } catch (error) {
-        this.error = 'Error adding type: ' + error.message;
-        console.error(this.error);
-      } finally {
-        this.isLoading = false;
-      }
-    },
+  //   async addType(newType) {
+  //     this.isLoading = true;
+  //     try {
+  //       await api.post('/types/add', newType);
+  //       this.fetchTypes(); 
+  //     } catch (error) {
+  //       this.error = 'Error adding type: ' + error.message;
+  //       console.error(this.error);
+  //     } finally {
+  //       this.isLoading = false;
+  //     }
+  //   },
 
-    async updateType(id, updatedType) {
-      this.isLoading = true;
-      try {
-        await api.put(`/types/update/${id}`, updatedType);
-        this.fetchTypes(); 
-      } catch (error) {
-        this.error = `Error updating type with ID ${id}: ` + error.message;
-        console.error(this.error);
-      } finally {
-        this.isLoading = false;
-      }
-    },
+  //   async updateType(id, updatedType) {
+  //     this.isLoading = true;
+  //     try {
+  //       await api.put(`/types/update/${id}`, updatedType);
+  //       this.fetchTypes(); 
+  //     } catch (error) {
+  //       this.error = `Error updating type with ID ${id}: ` + error.message;
+  //       console.error(this.error);
+  //     } finally {
+  //       this.isLoading = false;
+  //     }
+  //   },
 
-    async deleteType(id) {
-      this.isLoading = true;
-      try {
-        await api.delete(`/types/delete/${id}`);
-        this.fetchTypes(); 
-      } catch (error) {
-        this.error = `Error deleting type with ID ${id}: ` + error.message;
-        console.error(this.error);
-      } finally {
-        this.isLoading = false;
-      }
-    },
+  //   async deleteType(id) {
+  //     this.isLoading = true;
+  //     try {
+  //       await api.delete(`/types/delete/${id}`);
+  //       this.fetchTypes(); 
+  //     } catch (error) {
+  //       this.error = `Error deleting type with ID ${id}: ` + error.message;
+  //       console.error(this.error);
+  //     } finally {
+  //       this.isLoading = false;
+  //     }
+  //   },
   },
 });
 
